b56ef6d5f767914e4cdcb9a15dac13cd01280d0b
Yurii <y.sem@gmail.comgit config --global user.email y.sem@gmail.com>
2023-02-02 15:03:43 +0200

    coolscript.sh

---
+++ coolscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+COUNTER=0
+
+while [ $COUNTER -lt 10 ]; do
+	echo "Current counter is $COUNTER"
+	COUNTER=$(($COUNTER+1))  
+	# let COUNTER=COUNTER+1
+	# let COUNTER+=1
+done
+
+for dir1 in `ls *.txt`; do
+	cat $dir1
+done
+
+for x in {1..10}; do
+	echo "X = $x"
+done
+
+for (( i=1; i<=10; i++ )); do
+	echo "Nomer I = $i"
+done
